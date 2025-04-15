60e0c2f02dc043bc10d5bc6983e531e044d81127
AttackOnBreakfast <filippo3@msu.edu>
2025-04-15 10:57:00 -0400

    Temperature Calculator aligned with how galaxy_temp.py is structured

--- temperaturecalculator.py
+++ temperaturecalculator.py
@@ -4,28 +4,25 @@ import numpy
 
 
 class TemperatureCalculator:
-    def __init__(self, U: GalaxyImage, B: GalaxyImage, V: GalaxyImage) -> None:
-        self.U_image: GalaxyImage = U
-        self.B_image: GalaxyImage = B
-        self.V_image: GalaxyImage = V
+    def __init__(self, r_band_image: GalaxyImage, g_band_image: GalaxyImage, z_band_image: GalaxyImage) -> None:
+        self.r_band_image = r_band_image
+        self.g_band_image = g_band_image
+        self.z_band_image = z_band_image
 
-    def calculate_temperature(self, U_V: float, B_V: float) -> float:
-        return U_V - B_V
+    def calculate_temperature(self, r_minus_z_value: float, g_minus_z_value: float) -> float:
+        return r_minus_z_value - g_minus_z_value
 
     def compute_temperature_image(self) -> GalaxyImage:
-        new_image = numpy.zeros(self.U_image.shape)
-        for y in range(self.U_image.shape[0]):
-            for x in range(self.U_image.shape[1]):
-                new_image[x, y] = self.calculate_temperature(
-                    self.U_V_image[x, y],
-                    self.B_V_image[x, y],
+        r_minus_z_image = self.r_band_image - self.z_band_image
+        g_minus_z_image = self.g_band_image - self.z_band_image
+
+        temperature_array = np.zeros(self.r_band_image.shape)
+
+        for y in range(self.r_band_image.shape[0]):
+            for x in range(self.r_band_image.shape[1]):
+                temperature_array[x, y] = self.calculate_temperature(
+                    r_minus_z_image[x, y],
+                    g_minus_z_image[x, y]
                 )
-        return GalaxyImage(Image(new_image))
 
-    @property
-    def U_V_image(self) -> GalaxyImage:
-        return self.U_image - self.V_image
-
-    @property
-    def B_V_image(self) -> GalaxyImage:
-        return self.B_image - self.V_image
+        return GalaxyImage(Image(temperature_array))
