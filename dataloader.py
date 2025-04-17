ac223866a19281a9cba58d85dcfe2f40a9ce1996
Tormalao <tormalao@msu.edu>
2025-04-16 20:02:31 -0400

    Commiting dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,18 @@
 from galaxyloader import GalaxyLoader
 
 from typing import Generator
+import h5py
+import numpy
+import random
 
 
 class DataLoader:
-    def __init__(self, dataset_path: str = "dataset/"):
+    def __init__(self, dataset_path: str = "dataset/Dataset.h5"):
         self.load_path = dataset_path
 
     def load_all_galaxies(self) -> Generator:
-        for galaxy_name in range(10):
-            yield galaxy_name, GalaxyLoader(str(galaxy_name), self.load_path)
+        dataset: h5py.File = h5py.File(self.load_path, 'r')
+        classification: numpy.ndarray[int] = dataset["ans"]
+        filtered_galaxies = [index for index, galaxy_class in enumerate(classification) if int(galaxy_class) in (6, 7)]
+        for galaxy_number in random.choices(filtered_galaxies, k=20):
+            yield galaxy_number, GalaxyLoader(galaxy_number, self.load_path)
