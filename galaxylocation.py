d5c607182b484a40f2e351df4f028427a6365b29
ryanyu <yuryan@msu.edu>
2025-04-21 23:20:27 -0400

    Added class docstring to galaxylocation.

--- galaxylocation.py
+++ galaxylocation.py
@@ -1,4 +1,13 @@
 class GalaxyLocation:
+    """
+    Galaxylocation
+
+    Stores the center and radius for a galaxy in an image.
+
+    Attributes:
+        center (tuple[int, int]): center of galaxy in (x, y) format.
+        radius (int): radius of galaxy region.
+    """
     def __init__(self, center: tuple[int, int], radius: int) -> None:
         self.center: tuple[int, int] = center
         self.radius: int = radius
