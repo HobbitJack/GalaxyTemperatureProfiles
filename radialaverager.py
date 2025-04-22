e02f792920ba2b77271ab6d2163a60b298f79350
ryanyu <yuryan@msu.edu>
2025-04-21 23:18:34 -0400

    Added class and method docstrings to radialaverager.

--- radialaverager.py
+++ radialaverager.py
@@ -2,6 +2,15 @@ import numpy
 
 
 class RadialAverager:
+    """
+    RadialAverager
+
+    Computes average value for each radius across angles in a polar image.
+
+    Attributes:
+        radial_data (numpy.ndarray): 2D array of radial data,
+        containing angles in the rows, and radii in the columns.
+    """
     def __init__(self, radial_data: numpy.ndarray) -> None:
         if radial_data.shape[0] != 360:
             print("Radial data must span 360 degrees.")
@@ -10,4 +19,10 @@ class RadialAverager:
         self.radial_data: numpy.ndarray = radial_data
 
     def compute_average(self) -> list[float]:
+        """
+        Computes average value for each radius across all angles.
+
+        Returns:
+            list[float]: Average radial values for each position.
+        """
         return [float(x) for x in numpy.nanmean(self.radial_data, 0)]
